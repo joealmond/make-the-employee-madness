974b9640d12f0c05e3fad01355354f6419460a02
joealmond <jozsef.mandula@gmail.com>
2023-07-19 00:21:31 +0200

    implemented update record

--- client/src/Pages/EmployeeUpdater.jsx
+++ client/src/Pages/EmployeeUpdater.jsx
@@ -1,11 +1,95 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useState, useEffect } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
 
 const EmployeeUpdater = () => {
-    const { id } = useParams();
-  return (
-    <div>{id}</div>
-  )
-}
+  const [employee, setEmployee] = useState(null);
+  const [name, setName] = useState("");
+  const [level, setLevel] = useState("");
+  const [position, setPosition] = useState("");
+  const navigate = useNavigate();
+  const { id } = useParams();
 
-export default EmployeeUpdater
+  useEffect(() => {
+    const fetchEmployees = async () => {
+      const res = await fetch("/api/employees");
+      const data = await res.json();
+      const emplyeeSelected = data.filter((employee) => employee._id === id)[0]
+      setEmployee(emplyeeSelected)
+      setName(emplyeeSelected.name)
+      setLevel(emplyeeSelected.level)
+      setPosition(emplyeeSelected.position)
+    };
+    fetchEmployees();
+  }, [id]);
+
+  const handleUpdateSubmit = async (e) => {
+    e.preventDefault();
+
+    const modifiedEmployee = {
+        ...employee,
+        name,
+        level,
+        position
+    }
+
+    const res = await fetch(`/api/employees/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(modifiedEmployee),
+    });
+
+    const updated = await res.json();
+
+    if (!res.ok) return "Could not update record.";
+
+    navigate("/");
+  };
+
+  return (
+    <form className="EmployeeForm" onSubmit={handleUpdateSubmit}>
+      <h2>Update Employee</h2>
+      <div className="control">
+        <label>
+          Name:
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </label>
+      </div>
+
+      <div className="control">
+        <label>
+          Level:
+          <input
+            type="text"
+            value={level}
+            onChange={(e) => setLevel(e.target.value)}
+          />
+        </label>
+      </div>
+
+      <div className="control">
+        <label>
+          Position:
+          <input
+            type="text"
+            value={position}
+            onChange={(e) => setPosition(e.target.value)}
+          />
+        </label>
+      </div>
+      <div className="buttons">
+        <button type="submit">Update</button>
+        <Link to={"/"}>
+          <button>Cancel</button>
+        </Link>
+      </div>
+    </form>
+  );
+};
+
+export default EmployeeUpdater;
